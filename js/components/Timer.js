432adcecd6bef3200607e91e1400705f61f2b488
Matthew Daly <matthewbdaly@gmail.com>
2016-01-03 20:01:54 +0000

    Added a class to the timer component

--- js/components/Timer.js
+++ js/components/Timer.js
@@ -5,7 +5,7 @@ export default React.createClass({
   mixins: [PureRenderMixin],
   render: function () {
     return (
-      <button className="btn btn-danger btn-lg btn-block">{ this.props.title } - { this.props.count }</button>
+      <button className="timer btn btn-danger btn-lg btn-block">{ this.props.title } - { this.props.count }</button>
     );
   }
 });
