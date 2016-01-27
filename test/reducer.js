0db4cb9ccbed055de9ebcf26b1bd093389a5de17
Matthew Daly <matthewbdaly@gmail.com>
2016-01-27 21:24:06 +0000

    Failing test in place for reducer

---
+++ test/reducer.js
@@ -0,0 +1,22 @@
+import {List, Map, fromJS} from 'immutable';
+
+import reducer from '../js/reducer';
+
+describe('reducer', () => {
+  it('handles ADD_TIMER', () => {
+    const initialState = List();
+    const action = {
+      type: 'ADD_TIMER',
+      state: Map({
+        name: 'My timer',
+        count: 0
+      })
+    };
+    const nextState = reducer(initialState, action);
+
+    expect(nextState).to.equal(fromJS(
+      [{name: 'My timer', count: 0}]
+    ));
+
+  });
+});
