1c8dd0196d27395189a82136004c6abab0c14e03
Matthew Daly <matthewbdaly@gmail.com>
2016-01-31 16:30:44 +0000

    Can now toggle timer

--- js/reducer.js
+++ js/reducer.js
@@ -4,10 +4,21 @@ function addTimer(state, timer) {
   return state.push(timer);
 }
 
+function toggleTimer(state, name) {
+  return state.update(
+    state.findIndex(function (item) {
+      return item.get('name') == name;
+    }), function (item) {
+      return item.set('active', !item.get('active'));
+    });
+}
+
 export default function(state = List(), action) {
   switch (action.type) {
     case 'ADD_TIMER':
       return addTimer(state, action.state);
+    case 'TOGGLE_TIMER':
+      return toggleTimer(state, action.name);
   }
   return state;
 }
--- test/reducer.js
+++ test/reducer.js
@@ -21,4 +21,20 @@ describe('reducer', () => {
     ));
 
   });
+
+  it('handles TOGGLE_TIMER', () => {
+    const initialState = fromJS(
+      [{name: 'My timer', active: false, count: 0}]
+    );
+    const action = {
+      type: 'TOGGLE_TIMER',
+      name: 'My timer'
+    };
+    const nextState = reducer(initialState, action);
+
+    expect(nextState).to.equal(fromJS(
+      [{name: 'My timer', active: true, count: 0}]
+    ));
+
+  });
 });
