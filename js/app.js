12a78f3f0c09714d92a8e96c4e05603565bc7fac
Matthew Daly <matthewbdaly@gmail.com>
2016-02-05 22:51:07 +0000

    Set up store

--- js/app.js
+++ js/app.js
@@ -2,8 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Page from './components/Page';
 import styles from '../scss/style.scss';
+import {createStore} from 'redux';
+import reducer from './reducer';
+import {Provider} from 'react-redux';
+
+const store = createStore(reducer);
 
 ReactDOM.render(
-  <Page />,
+  <Provider store={store}>
+    <Page />
+  </Provider>,
   document.getElementById('view')
 );
